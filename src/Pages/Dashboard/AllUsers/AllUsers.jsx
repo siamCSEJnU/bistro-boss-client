9f30089c3c8c5d45bbe9902ca0026dce5eae4abd
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-31 00:08:16 +0600

    admin creating functionality added

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,12 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const { data: users = [], refetch } = useQuery(["users"], async () => {
     const res = await fetch("http://localhost:5000/users");
     return res.json();
   });
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is Admin now!!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
 
   const handleDelete = (user) => {};
   return (
@@ -39,7 +58,10 @@ const AllUsers = () => {
                   {user.role === "admin" ? (
                     "admin"
                   ) : (
-                    <button className="btn btn-ghost bg-orange-300">
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-ghost bg-orange-300"
+                    >
                       <FaUserShield></FaUserShield>
                     </button>
                   )}
