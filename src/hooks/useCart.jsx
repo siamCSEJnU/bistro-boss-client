9c170af2a2c5742cd8d1b1e51c58a28e497dcea5
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-31 01:42:51 +0600

    jwt verfication added

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,15 +1,22 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
+import { getItem } from "localforage";
 
 const useCart = () => {
   const { user } = useContext(AuthContext);
+  const token = getItem("access-token");
 
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/carts?email=${user.email}`
+        `http://localhost:5000/carts?email=${user.email}`,
+        {
+          headers: {
+            auhtorization: `bearer ${token}`,
+          },
+        }
       );
       return res.json();
     },
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -49,6 +50,19 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+
+      //set and get token
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((data) => {
+            // console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
+          });
+      } else {
+        localStorage.getItem("access-token");
+      }
+
       setLoading(false);
     });
     return () => {
