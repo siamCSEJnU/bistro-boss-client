d3d8b19a1c384dfcaf406a493d5dded3ad33b700
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-06-03 02:13:08 +0600

    add item funcitonality with axiossecure and image hosting with imagebb done

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,12 +1,132 @@
+import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
+const image_hosting_token = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
 
 const AddItem = () => {
+  const [axiosSecure] = useAxiosSecure();
+  const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_token}`;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+    fetch(image_hosting_url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imageResponse) => {
+        if (imageResponse.success) {
+          const imgURL = imageResponse.data.display_url;
+          //In DB there is menu collection with image field
+          //   const newItem = data;
+          //   newItem.image = imgURL;  or,
+
+          const { name, recipe, category, price } = data;
+          const newItem = {
+            name,
+            recipe,
+            category,
+            price: parseFloat(price),
+            image: imgURL,
+          };
+          console.log(newItem);
+          axiosSecure.post("/menu", newItem).then((data) => {
+            console.log(data.data);
+            if (data.data.insertedId) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Item has been added successfuly",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
+        }
+      });
+  };
+  console.log(errors);
   return (
-    <div className="w-full">
+    <div className="w-full ">
       <SectionTitle
         heading={"Add an Item"}
         subheading={"What's New?"}
       ></SectionTitle>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-3 px-10">
+        <div className="form-control w-full ">
+          <label className="label">
+            <span className="label-text font-semibold">Recipe Name*</span>
+          </label>
+          <input
+            {...register("name", { required: true, maxLength: 120 })}
+            //register the name with which it is stored in mongodb
+            type="text"
+            placeholder="recipe name"
+            className="input input-bordered w-full "
+          />
+        </div>
+        <div className="flex gap-5">
+          <div className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">Category*</span>
+            </label>
+            <select
+              defaultValue={"Pick One"}
+              {...register("category", { required: true })}
+              className="select select-bordered"
+            >
+              <option disabled>Pick one</option>
+              <option>Pizza</option>
+              <option>Soup</option>
+              <option>Salad</option>
+              <option>Drinks</option>
+              <option>Deshi</option>
+              <option>Dessert</option>
+            </select>
+          </div>
+          <div className="form-control w-full ">
+            <label className="label">
+              <span className="label-text font-semibold">Price*</span>
+            </label>
+            <input
+              {...register("price", { required: true })}
+              type="number"
+              placeholder="price"
+              className="input input-bordered w-full "
+            />
+          </div>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Recipe Details</span>
+          </label>
+          <textarea
+            {...register("recipe", { required: true })}
+            className="textarea textarea-bordered h-24"
+            placeholder="Bio"
+          ></textarea>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Item Image*</span>
+          </label>
+          <input
+            {...register("image", { required: true })}
+            type="file"
+            className="file-input file-input-bordered w-full max-w-xs"
+          />
+        </div>
+        <input type="submit" value="Add Item" className="btn btn-sm" />
+      </form>
     </div>
   );
 };
